0a2f48caff3588a485924d7812aea0583042a418
liuliu01 <782202048@qq.com>
2017-03-16 18:32:26 +0800

    wcf

---
+++ WCFDemo/IService1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFDemo
+{
+    // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码和配置文件中的接口名“IService1”。
+    [ServiceContract]                  //服务契约
+    public interface IService1
+    {
+
+        [OperationContract]      //服务契约
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        [OperationContract]
+        student1 getStudent(student1 student);
+        // TODO: 在此添加您的服务操作
+    }
+
+
+    // 使用下面示例中说明的数据约定将复合类型添加到服务操作。
+    [DataContract]        //数据契约 datacontract 
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+    [DataContract]
+    public class student1
+    {
+        string string1 = "heh";
+        [DataMember]
+        public string String1
+        {
+            get
+            {
+                return string1;
+            }
+            private set { string1 = value; }
+        }
+    }
+}
